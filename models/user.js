769449f5488587ca9e22832aeab18f99875dd287
RyuJeongHa <fbwjdgk00@bu.ac.kr>
2021-10-18 11:55:49 +0900

    처음 저장소에 올림

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('momgoose');
+const { string, number } = require('prop-types');
+
+const userSchema = mongoose.userSchema({
+    nickname:{
+        type:String,
+        maxlength: 20,
+        unique:1 //똑같은 이름을 쓰지 못하도록 하는 것
+    },
+    password:{
+        type: String,
+        minlength: 5
+    },
+    token:{
+        type:String
+    },
+    tokenExp:{
+        type:Number,
+    },//토근을 사용할 수 있는 유효기간
+})
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = {user}// 모듈을 다른 곳에서도 쓸 수 있음
